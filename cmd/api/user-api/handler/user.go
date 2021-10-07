613babce9c0f49695a9977ef7131ce878b7ef4d8
Viet Nguyen <vietnh46@ghtk.co>
2021-10-07 10:25:30 +0700

    dd different error handling for data not found

--- cmd/api/user-api/handler/user.go
+++ cmd/api/user-api/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"ktrain/cmd/api/user-api/mapper"
 	"ktrain/cmd/repository"
+	"ktrain/pkg/errors"
 	"ktrain/pkg/httputil"
 	"net/http"
 )
@@ -21,6 +22,11 @@ func (h *userHandler) GetMyProfile(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	user, err := h.userRepository.GetUserByID(ctx.Value("userID").(int64))
 	if err != nil {
+		if errors.IsDataNotFound(err) {
+			httputil.RespondError(w, http.StatusNotFound, "Your profile not found")
+			return
+		}
+
 		httputil.RespondError(w, http.StatusInternalServerError, "Error when getting user profile")
 		return
 	}
