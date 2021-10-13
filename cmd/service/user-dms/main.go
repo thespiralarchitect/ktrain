5f77dcd913e6c537b632f4c8eac4d3ff7849d83d
tovinhtuan <tuan.tv100698@gmail.com>
2021-10-13 10:20:05 +0700

    clean code

--- cmd/service/user-dms/main.go
+++ cmd/service/user-dms/main.go
@@ -40,7 +40,8 @@ func main() {
 	userRepository := repository.NewUserRepository(psqlDB)
 	h, err := handlers.NewUserHandler(userRepository)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error when creae new user handler, err: %v", err)
+		return
 	}
 	reflection.Register(s)
 	pb.RegisterUserDMSServiceServer(s, h)
